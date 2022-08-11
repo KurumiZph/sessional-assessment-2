9c42b3462d46addaabdf63b09fe75d0470774225
Arish-008 <80612408+Arish-008@users.noreply.github.com>
2022-08-11 18:15:16 +0530

    Added TimeStamps

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 const xhr = new XMLHttpRequest();
 
-const searchValue = document.querySelector('#my-text').value
-
 const url = `https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09`
 
 xhr.open('GET', url)
@@ -11,17 +9,27 @@ xhr.onreadystatechange = () => {
         const response = JSON.parse(xhr.responseText)
         console.log(response)
 
+        let m=9,y=2021
         let template = ''
         for(let i=0; i < response.length; i++) {
-            template += `
-                <div class="blogs">
-                    ${response.userId[i].id[i]}
-                </div>
-            `
+            if(i % 10 ==0){
+                template +=`<h2 class="post-date">Posted: ${m}/${y}</h2>`
+                m+=1
+                if(m>12){
+                    m=m-12
+                    y+=1
+                }
+            }
+                template += 
+                    `
+                    <div class="blog-items" style="width: 100%; padding: 12px; margin: 1em;">
+                    <h3 class="blog-title">${response[i].title}</h3>
+                    <p class="blog-body">${response[i].body}</p>
+                    <a href="#" class="blog-rm">[Read More]</a>
+                    </div>
+                    `
+            }
+            document.querySelector('#my-container').innerHTML = template
         }
-
-        document.querySelector('#my-container').innerHTML = template
     }
-}
-
 xhr.send()
